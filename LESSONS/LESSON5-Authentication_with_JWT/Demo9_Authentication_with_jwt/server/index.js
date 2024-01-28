6fa4f82800ccea0feb6b13b3f35793f82c5f650d
Netanel <netanel77777@gmail.com>
2024-01-28 14:56:50 +0200

    Lesson4 - JsonWebToken(JWT)

---
+++ LESSONS/LESSON5-Authentication_with_JWT/Demo9_Authentication_with_jwt/server/index.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import DbConnection from './configs/dbConnection.js';
+
+import authRouter from "./controller/auth.js";
+import productsRouter from "./controller/products.js";
+
+// Check first if the port defined in the .env file, if not, assign 3001
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+DbConnection();
+
+app.use("/auth", authRouter);
+app.use("/products", productsRouter);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
