345119736547dabcac8261e1a73c6d984021a630
Netanel <netanel77777@gmail.com>
2025-03-06 15:17:22 +0200

    Add movie page

--- FullStack/Cinema/client/src/components/pages/movies/addMovie.jsx
+++ FullStack/Cinema/client/src/components/pages/movies/addMovie.jsx
@@ -1,8 +1,32 @@
-import styles from './allMovies.module.css';
+import styles from './addMovie.module.css';
+
+import { useRef } from 'react';
+
+import Input from '../../genericComp/input';
+import Button from '../../UI/button/button';
 
 const AddMovie = () => {
+  const movieNameRef = useRef();
+  const genresRef = useRef();
+  const imageRef = useRef();
+  const premieredRef = useRef();
+
+  const saveMovieHandler = (event) => {};
+
+  const cancelHandler = () => {};
+
   return (
-    <div>Add movie</div>
+    <form onSubmit={saveMovieHandler} className={styles.add_movie_form_container}>
+      <Input title="Name" type='text' className="" value="" ref={movieNameRef}/>
+      <Input title="Genres" type='text' className="" value="" ref={genresRef}/>
+      <Input title="Image URL" type='text' className="" value="" ref={imageRef}/>
+      <Input title="Premiered" type='text' className="" value="" ref={premieredRef}/>
+
+      <div className={styles.add_movie_actions}>
+        <Button className={styles.add_movie_button} text="Save" type="submit"/>
+        <Button className={styles.add_movie_button} text="Cancle" type="button" onClick={cancelHandler}/>
+      </div>
+    </form>
   )
 }
 
